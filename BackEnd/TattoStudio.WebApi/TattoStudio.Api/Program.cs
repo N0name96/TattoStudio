de26d6338a6e0b038d2b2b27176595bc26cfd90f
N0name <N0name>
2026-06-20 23:16:04 +0200

    update create tables in first build

--- BackEnd/TattoStudio.WebApi/TattoStudio.Api/Program.cs
+++ BackEnd/TattoStudio.WebApi/TattoStudio.Api/Program.cs
@@ -1,7 +1,12 @@
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using TattoStudio.Api.Controllers;
 using TattoStudio.Api.Middleware;
 using TattoStudio.Application;
 using TattoStudio.Infrastructure;
+using TattoStudio.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +20,20 @@ builder.Services.AddProblemDetails();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<TattoStudioDbContext>();
+    var creator = db.Database.GetService<IRelationalDatabaseCreator>() as RelationalDatabaseCreator;
+    try
+    {
+        await creator!.CreateTablesAsync();
+    }
+    catch (DbException ex) when (ex.SqlState == "42P07")
+    {
+        // Las tablas ya existen, no es necesario crearlas
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
